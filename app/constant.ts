fc7043a182711068f3fd1df48d1f1591e955e84b
13705412 <127035472+13705412@users.noreply.github.com>
2023-03-29 18:09:05 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
-export const OWNER = "Yidadaa";
+export const OWNER = "qq5666157";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const UPDATE_URL = `${REPO_URL}#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
+export const REPO_URL = `https://www.i5.chat`;
+export const UPDATE_URL = ``;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
